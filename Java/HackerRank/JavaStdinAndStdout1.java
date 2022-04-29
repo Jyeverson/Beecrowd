2b88e187d5cc93533176d7dc467e0b4ccd27482a
Jyeverson <jyeversonsantos@gmail.com>
2022-04-29 01:56:26 -0300

    HackerRank solved in Java

---
+++ Java/HackerRank/JavaStdinAndStdout1.java
@@ -0,0 +1,26 @@
+/**
+ Task
+In this challenge, you must read  integers from stdin and then print them to stdout. 
+Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+
+There are  lines of input, and each line contains a single integer.
+ */
+
+import java.util.*;
+import java.util.Scanner;
+public class JavaStdinAndStdout1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        scan.close();
+    }
+}
