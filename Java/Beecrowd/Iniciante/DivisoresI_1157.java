6eadcfee844a8ab2e7a420f580ca6d4078a36af1
jyeverson <jyeversonsantos@gmail.com>
2022-06-06 19:15:01 -0300

    novo problema resolvido

---
+++ Java/Beecrowd/Iniciante/DivisoresI_1157.java
@@ -0,0 +1,19 @@
+/*
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+ */
+import java.util.Scanner;
+ public class DivisoresI_1157 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt();     
+        for(int i = 1; i <= N; i++){
+            if( N % i == 0)System.out.println(i);
+        }
+    }
+}
