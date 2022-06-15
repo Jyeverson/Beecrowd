3f8d5803d4e0f668d06d21d1e21fd1e3cafa0502
jyeverson <jyeversonsantos@gmail.com>
2022-06-15 11:21:16 -0300

    novos exercicios resolvidos

---
+++ Kotlin/BobConduite_1589.kt
@@ -0,0 +1,33 @@
+/*
+Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o 
+segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que 
+ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
+
+
+
+Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois 
+círculos, qual o raio do menor círculo que possa englobar ambos os dois?
+
+Entrada
+Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de 
+teste.
+
+Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os 
+respectivos raios. Os inteiros serão positivos e todas as contas caberão em um 
+inteiro normal de 32 bits.
+Saída
+Em cada caso, imprima o menor raio possível em uma única linha
+ */
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val T = sc.nextInt()
+    for (i in 0 until T) {
+        val R1 = sc.nextInt()
+        val R2 = sc.nextInt()
+        println(R1 + R2)
+    }
+    sc.close()
+}
