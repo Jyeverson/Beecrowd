c6339424c079e953d40872a9fb4d9ab0f40daf94
jyeverson <jyeversonsantos@gmail.com>
2023-01-25 15:23:23 -0300

    novo exercicio resolvido

--- Java/TempoDeUmEvento_1061.java
+++ Java/TempoDeUmEvento_1061.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class TempoDeUmEvento_1061 {
-	
+
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
         int totalDias = 0, totalHoras = 0, totalMinutos = 0, totalSegundos = 0;
@@ -40,46 +40,46 @@ public class TempoDeUmEvento_1061 {
         int horaFinal = Integer.parseInt(ShoraFinal[0]);
         int minutoFinal = Integer.parseInt(ShoraFinal[1]);
         int segundoFinal = Integer.parseInt(ShoraFinal[2]);
-        
+
         if (segundoFinal < segundoInicial) {
-        	totalSegundos += (60 - segundoInicial) + segundoFinal;
-        	totalMinutos--;
+            totalSegundos += (60 - segundoInicial) + segundoFinal;
+            totalMinutos--;
         } else if (segundoFinal > segundoInicial) {
-        	totalSegundos += segundoFinal - segundoInicial;
+            totalSegundos += segundoFinal - segundoInicial;
         } else {
-        	totalSegundos = 0;
+            totalSegundos = 0;
         }
-        
+
         if (minutoFinal < minutoInicial) {
-        	totalMinutos += (60 - minutoInicial) + minutoFinal;
-        	totalHoras--;
-        } else if (minutoFinal > minutoInicial)  {
-        	totalMinutos += minutoFinal - minutoInicial;
+            totalMinutos += (60 - minutoInicial) + minutoFinal;
+            totalHoras--;
+        } else if (minutoFinal > minutoInicial) {
+            totalMinutos += minutoFinal - minutoInicial;
         } else {
-        	totalMinutos = 0;
+            totalMinutos = 0;
         }
-        
+
         if (horaFinal < horaInicial) {
-        	totalHoras += (24 - horaInicial) + horaFinal;
-        	totalDias--;
+            totalHoras += (24 - horaInicial) + horaFinal;
+            totalDias--;
         } else if (horaFinal > horaInicial) {
-        	totalHoras += horaFinal - horaInicial;
+            totalHoras += horaFinal - horaInicial;
         } else {
-        	totalHoras = 0;
+            totalHoras = 0;
         }
-        
+
         if (diaFinal < diaInicial) {
-        	totalDias += (30 - diaInicial) + diaFinal;
+            totalDias += (30 - diaInicial) + diaFinal;
         } else if (diaFinal > diaInicial) {
-        	totalDias += diaFinal - diaInicial;
+            totalDias += diaFinal - diaInicial;
         } else {
-        	totalDias = 0;
+            totalDias = 0;
         }
-                
+
         System.out.println(totalDias + " dia(s)");
         System.out.println(totalHoras + " hora(s)");
         System.out.println(totalMinutos + " minuto(s)");
         System.out.println(totalSegundos + " segundo(s)");
     }
-	
+
 }
