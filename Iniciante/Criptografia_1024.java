e1ebe89700936bed1aaefa3b4759b81ca279b511
jyeverson <jyeversonsantos@gmail.com>
2023-02-01 23:09:10 -0300

    Categoria

---
+++ Iniciante/Criptografia_1024.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Criptografia_1024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char texto = nextLine();
+        
+
+        sc.close();
+    }
+}
