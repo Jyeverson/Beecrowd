4f4d4257df01ca5f80a1c800d661fc20f920c4c8
Jyeverson Madalozo <71305121+Jyeverson@users.noreply.github.com>
2023-04-25 18:33:01 -0300

    novo exercicio resolvido

--- src/_1059.java
+++ src/_1059.java
@@ -1,6 +1,7 @@
 public class _1059 {
     public static void main(String[] args) {
         int contador = 1;
+
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
                 contador++;
---
+++ src/_1168.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class _1168 {
+    public static void main(String[] args) {
+        int[] leds = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String numero = sc.nextLine();
+            int total = 0;
+            for (int j = 0; j < numero.length(); j++) {
+                int digito = Character.getNumericValue(numero.charAt(j));
+                total += leds[digito];
+            }
+            System.out.println(total + " leds");
+        }
+
+        sc.close();
+    }
+}
